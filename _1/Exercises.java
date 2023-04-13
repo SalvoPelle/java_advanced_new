f5b809a61a0ec9877027de9097afab0b33c16312
Salvo <pellegrino.salvatore02@gmail.com>
2023-04-13 17:40:51 +0200

    Add exercise _1

---
+++ _1/Exercises.java
@@ -0,0 +1,176 @@
+package javaadvanced._1;
+
+import java.util.*;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+    }
+
+    /**
+     * 1:
+     * <p>
+     * 1. Add some numbers (1, 1, 5, 2, 1) to the set using the add() method.
+     * 2. Remove '1' from  the set using the .remove(1)
+     * 3. Print the set again to confirm the element was removed.
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+        Set<Integer> ourSet = new HashSet<>();
+
+        // Add some numbers using the add method
+        // Your code
+        ourSet.add(1);
+        ourSet.add(1);
+        ourSet.add(5);
+        ourSet.add(2);
+        ourSet.add(1);
+
+//        if (ourSet.containsAll(Set.of(1, 1, 5, 2, 1))) {
+//            System.out.println("1a. Set contains all numbers");
+//        } else {
+//            System.out.println("1a. Set does not contain all numbers");
+//        } NON FUNZIONA. DA ERRORE CON containsAll(Set.of(...)) E NON PARTE IL RUN
+
+        if (ourSet.containsAll(Set.of(1, 5, 2))) {
+            System.out.println("1a. Set contains all numbers");
+        } else {
+            System.out.println("1a. Set does not contain all numbers");
+        }
+
+        System.out.println(ourSet);
+
+        // Remove the element '1'
+        // Your code
+        ourSet.remove(1);
+
+        System.out.println(ourSet);
+
+        if (ourSet.contains(1)) {
+            System.out.println("1b. The set still contains 1");
+        } else {
+            System.out.println("1b. The set does not contain 1");
+        }
+    }
+
+    /**
+     * 2:
+     * <p>
+     * Read the comments below and write comments on what you think our print statement will output!
+     */
+    private static void exercise2() {
+        Set<String> fruitsSet = new HashSet<>();
+
+        fruitsSet.add("apple");
+        fruitsSet.add("banana");
+        fruitsSet.add("orange");
+        fruitsSet.add("kiwi");
+        fruitsSet.add("mango");
+
+        fruitsSet.addAll(fruitsSet);
+
+        //2a. What will this output? [apple, banana, orange, kiwi, mango]   [banana, orange, apple, kiwi, mango] stamperà solo una volta i valori inseriti
+        System.out.println(fruitsSet);
+
+        List<String> fruitsList = List.of("apple", "banana", "grapes", "pear", "mango");
+
+        fruitsSet.addAll(fruitsList);
+
+        //2b. What will this output? [apple, banana, grapes, pear, mango, kiwi, orange] [banana, orange, apple, kiwi, pear, mango, grapes]
+                                        // stamperà una volta sola i valori doppioni e aggiunge i valori nuovi
+        System.out.println(fruitsSet);
+    }
+
+    /**
+     * 3: Write code to solve the following problems
+     */
+    private static void exercise3() {
+        // 3a. Find the min and max values in the Set below
+        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        //int max = Integer.MAX_VALUE;          // IL CODICE PRIMA ERA COSI'
+        //int min = Integer.MIN_VALUE;
+
+
+        int max = Integer.MIN_VALUE;    //CAMBIANDOLO IN QUESTO MODO FUNZIONA. UN ALTRO MODO CHE MI VIENE IN MENTE è SOLO RIASSEGNADO I VALORI NEL CICLO FOR
+        int min = Integer.MAX_VALUE;
+
+
+        for (Integer number : numbers) {
+            // Your code
+            if (max < number){
+                max = number;
+            }
+            if (min > number){
+                min = number;
+            }
+        }
+
+        // PROVA 2
+//        for (Integer number : numbers){
+//            max = Math.max(number, max);
+//            min = Math.min(number, min);
+//        }
+
+        if (max != 10 || min != 1) {
+            System.out.println("3a. Incorrect min or max values");
+        }
+
+        // 3b. Find which items from set a are also in set b, add these to itemsContainedInBothSets
+        //     use the `contains` method and "nested" for loop
+        Set<Integer> setA = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Set<Integer> setB = Set.of(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        Set<Integer> itemsContainedInBothSets = new HashSet<>();
+
+        // Your code
+        for (Integer a : setA){
+            for (Integer b : setB){// il secondo for è richiesto dall'esercizio ma non serve
+                if (setB.contains(a)){
+                    itemsContainedInBothSets.add(a);
+                }
+            }
+        }
+
+        if (!itemsContainedInBothSets.equals(Set.of(5, 6, 7, 8, 9, 10))) {
+            System.out.println("3b. Incorrect number of items in itemsContainedInBothSets");
+        } else {
+            System.out.println("3b. Correct number of items in itemsContainedInBothSets");
+        }
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     */
+    private static void exercise4() {
+        Set<Integer> orderedNumbers = new TreeSet<>();
+        orderedNumbers.addAll(Set.of(5, 3, 1, 2, 4, 6, 7, 8, 9, 10));
+        // 4a. Find the min value in the TreeSet with as few loops as possible,
+        //     you can use `break;` to exit the loop once you've found it!
+        int min = Integer.MAX_VALUE;
+        // Your code
+
+        // PROVA 1
+//        for (Integer lower : orderedNumbers){
+//            if (min>lower){
+//                min = lower;
+//            }
+//        }
+
+        // PROVA 2
+        for (Integer lower : orderedNumbers){
+            min = Math.min(min, lower);
+        }
+
+
+        if (min != 1) {
+            System.out.println("4a. Incorrect min value");
+        } else {
+            System.out.println("4a. Correct min value");
+        }
+
+    }
+}
