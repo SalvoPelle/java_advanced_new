608f9688c2a1da66c0bf01dc4985df510cb4b787
Salvo <pellegrino.salvatore02@gmail.com>
2023-04-18 09:25:37 +0200

    Exercise in production _1

---
+++ javaInProduction/_1/Person.java
@@ -0,0 +1,11 @@
+package javaadvanced.javaInProduction._1;
+
+public record Person(String name, int age, String address) {
+    @Override
+    public String toString() {
+        return "His name is " +
+                 name + '\'' +
+                ", is age is " + age +
+                " and he lives in " + address;
+    }
+}
